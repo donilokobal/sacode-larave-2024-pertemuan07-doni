b3c30d80341edd29b7abd2e305e7fd9b5af5fbb4
Doni <donilokobal@gail.com>
2024-08-29 20:00:41 +0900

    menambahkan commit

---
+++ JS/js script.js
@@ -0,0 +1,13 @@
+// single line comment
+
+/*
+multiple line comment
+bisa tuli juaga di sini
+*/
+
+var namaDepan = "Doni";
+var namaBelakang = "Lokobal";
+
+document
+.getElementById("nama")
+.innerHTML = namaDepan + namaBelakang;
